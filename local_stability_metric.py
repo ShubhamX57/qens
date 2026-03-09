22a993dfb9fc4adf53b20910f469d3eab27cf5ce
wilfb <wilf.bates11@gmail.com>
2026-03-09 10:43:57 +0000

    initalised stability metric

---
+++ local_stability_metric.py
@@ -0,0 +1,10 @@
+'''
+The aim of this file is to create a numerical representation of how useful a q point will be
+this will allow for easy selection 
+
+this will feed into the dashboard and use a piecewise cmap- with green to red marking good to bad
+
+'''
+
+
+def stability(q_value)
